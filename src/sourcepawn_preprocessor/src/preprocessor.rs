d3cd0634b53d4f5273a8995ed42a6eee41657e30
Sarrus1 <omegasix0611@gmail.com>
2023-04-19 14:32:19 +0200

    checkpoint

--- src/sourcepawn_preprocessor/src/preprocessor.rs
+++ src/sourcepawn_preprocessor/src/preprocessor.rs
@@ -11,6 +11,7 @@ pub struct SourcepawnPreprocessor<'a> {
     conditions_stack: Vec<bool>,
     out: Vec<String>,
     defines_map: FxHashMap<String, Vec<Symbol>>,
+    expansion_stack: Vec<Symbol>,
 }
 
 impl<'a> SourcepawnPreprocessor<'a> {
@@ -22,10 +23,19 @@ impl<'a> SourcepawnPreprocessor<'a> {
             conditions_stack: vec![],
             out: vec![],
             defines_map: FxHashMap::default(),
+            expansion_stack: vec![],
         }
     }
+
     pub fn preprocess_input(&mut self) -> String {
-        while let Some(symbol) = self.lexer.next() {
+        loop {
+            let symbol = if !self.expansion_stack.is_empty() {
+                self.expansion_stack.pop().unwrap()
+            } else if let Some(symbol) = self.lexer.next() {
+                symbol
+            } else {
+                break;
+            };
             if !self.conditions_stack.is_empty() && !*self.conditions_stack.last().unwrap() {
                 self.process_negative_condition(&symbol);
                 continue;
@@ -38,6 +48,17 @@ impl<'a> SourcepawnPreprocessor<'a> {
                     self.current_line = "".to_string();
                     self.prev_end = 0;
                 }
+                TokenKind::Identifier => match self.defines_map.get(&symbol.text()) {
+                    Some(_) => {
+                        self.push_ws(&symbol);
+                        self.expand_define(&symbol, 0);
+                    }
+                    None => {
+                        self.push_ws(&symbol);
+                        self.prev_end = symbol.range.end_col;
+                        self.current_line.push_str(&symbol.text());
+                    }
+                },
                 TokenKind::Eof => {
                     self.push_ws(&symbol);
                     self.push_current_line();
@@ -52,6 +73,18 @@ impl<'a> SourcepawnPreprocessor<'a> {
         }
 
         self.out.join("\n")
+    }
+
+    fn expand_define(&mut self, symbol: &Symbol, depth: u32) {
+        for sub_symbol in self.defines_map.get(&symbol.text()).unwrap() {
+            match &sub_symbol.token_kind {
+                TokenKind::Identifier => {
+                    self.expand_define(&sub_symbol, depth + 1);
+                }
+                TokenKind::Newline | TokenKind::LineContinuation => (),
+                _ => self.expansion_stack.push(sub_symbol.clone()),
+            }
+        }
     }
 
     fn process_directive(&mut self, dir: &PreprocDir, symbol: &Symbol) {
@@ -137,8 +170,10 @@ impl<'a> SourcepawnPreprocessor<'a> {
     }
 
     fn push_ws(&mut self, symbol: &Symbol) {
-        let ws_diff = symbol.range.start_col - self.prev_end;
-        self.current_line.push_str(&" ".repeat(ws_diff));
+        if symbol.range.start_col > self.prev_end {
+            self.current_line
+                .push_str(&" ".repeat(symbol.range.start_col - self.prev_end));
+        }
     }
 
     fn push_current_line(&mut self) {
